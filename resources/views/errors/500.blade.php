b3cc6b2562dbf4c1ed9130891dad91aaefaf3823
kolbyd <kolby.dunning@gmail.com>
2020-08-30 21:52:58 -0400

    nice looking 500 page

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,5 +1,18 @@
-@extends('errors::minimal')
+@extends('layouts.master')
 
 @section('title', __('Server Error'))
-@section('code', '500')
-@section('message', __('Server Error'))
+
+@section('content')
+    <div class="container py-5">
+        <h1 class="font-weight-bold"><i class="fa fa-cogs"></i>  Uh oh... (500 Server Error)</h1>
+        <div class="mt-4">
+            <h3>It appears our server didn't like that very much...</h3>
+            <h5>Please submit a ticket to our <a href="{{url('/dashboard/tickets?create=yes')}}">webmaster</a> with:</h5>
+            <ul class="h5">
+                <li>What you were trying to access</li>
+                <li>What URL you were on</li>
+                <li>Any other relevent information</li>
+            </ul>
+        </div>
+    </div>
+@stop
