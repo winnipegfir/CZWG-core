25b99b00fb04379f64307009bc013db1e1c04de9
natopower <natopower@gmail.com>
2020-10-10 00:23:35 -0300

    oops forgot to fix the migration typo gg

--- database/migrations/2020_10_10_025558_create_trianing_wait_times_table.php
+++ database/migrations/2020_10_10_025558_create_trianing_wait_times_table.php
@@ -13,8 +13,10 @@ class CreateTrianingWaitTimesTable extends Migration
      */
     public function up()
     {
-        Schema::create('trianing_wait_times', function (Blueprint $table) {
+        Schema::create('training_waittimes', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->text('wait_length');
+            $table->text('colour');
             $table->timestamps();
         });
     }
@@ -26,6 +28,6 @@ class CreateTrianingWaitTimesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('trianing_wait_times');
+        Schema::dropIfExists('training_waittimes');
     }
 }
