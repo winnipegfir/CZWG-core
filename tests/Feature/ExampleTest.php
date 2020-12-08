0d870654ebfef433f92f4a242660fba2dca4a674
kolbyd <kolby.dunning@gmail.com>
2020-12-07 22:59:57 -0500

    Let's get some tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,11 +2,13 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use DatabaseMigrations;
     /**
      * A basic test example.
      *
@@ -14,6 +16,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
+        $this->seed();
         $response = $this->get('/');
 
         $response->assertStatus(200);
