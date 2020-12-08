b31196ff9d04f23061216537c2aa1bdf188e7ea4
kolbyd <58011171+kolbyd@users.noreply.github.com>
2020-12-07 23:10:00 -0500

    Update 2020_08_27_000000_update_users_table.php

--- database/migrations/2020_08_27_000000_update_users_table.php
+++ database/migrations/2020_08_27_000000_update_users_table.php
@@ -49,7 +49,7 @@ class UpdateUsersTable extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->string('fname')->nullable(false)->change();
             $table->string('lname')->nullable(false)->change();
-        }
+        });
     }
 
     /**
