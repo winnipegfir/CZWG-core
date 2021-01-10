49394ef2cef9a8fe8477d932ea00b961dcde4843
James White <jamesdwhite95@gmail.com>
2021-01-10 17:36:18 -0500

    Add column

---
+++ database/migrations/2021_01_10_223412_add_assignall_to_cbt_modules_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddAssignallToCbtModulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cbt_modules', function (Blueprint $table) {
+                $table->int('assignall')
+                    ->after('cbt_exam_id')
+                    ->default('0');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cbt_modules', function (Blueprint $table) {
+            //
+        });
+    }
+}
