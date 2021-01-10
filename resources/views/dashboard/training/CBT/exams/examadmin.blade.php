816792359e59b7744ef93af621aa45eb7d07f85b
natopower <natopower@gmail.com>
2021-01-09 23:52:33 -0400

    exam styling

--- resources/views/dashboard/training/CBT/exams/examadmin.blade.php
+++ resources/views/dashboard/training/CBT/exams/examadmin.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.master')
 
 @section('navbarprim')
+@section('title', 'Exam Admin')
 
     @parent
 
--- resources/views/dashboard/training/CBT/exams/startexam.blade.php
+++ resources/views/dashboard/training/CBT/exams/startexam.blade.php
@@ -11,7 +11,7 @@
     <div class="container" style="margin-top: 20px;">
     <a href="{{route('cbt.exam')}}" class="blue-text" style="font-size: 1.2em;"> <i class="fas fa-arrow-left"></i> Back to Exam Centre</a>
     <br></br>
-        <div class="jumbotron">
+        <div class="jumbotron" style="padding-bottom: 2%; padding-top: 3%">
             <h3 class="font-weight-bold blue-text">{{$subject->name}}</h3>
             <h5><text class="font-weight-bold">Your Instructor:</text> {{$student->instructor->user->fullName('FL')}}</h5>
             <p>The passing mark for this exam is <text class="font-weight-bold text-success">80%.</text> Good luck!</p>
