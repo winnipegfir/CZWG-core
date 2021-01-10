a0015dcbc77ec58fe838ebba253f486d634a8eee
James White <jamesdwhite95@gmail.com>
2021-01-10 17:17:25 -0500

    Bug Fix

--- routes/web.php
+++ routes/web.php
@@ -266,7 +266,7 @@ Route::group(['middleware' => 'auth'], function () {
             Route::get('/module/admin/{id}', 'AtcTraining\CBTController@viewAdminModule')->name('cbt.module.view.admin');
             Route::get('/module/delete/{id}', 'AtcTraining\CBTController@deleteModule')->name('cbt.module.delete');
             Route::post('/lesson/add/{id}', 'AtcTraining\CBTController@addLesson')->name('cbt.lesson.add');
-            Route::get('/lesson/e/{id}', 'AtcTraining\CbtController@editLesson')->name('cbt.lesson.edit');
+            Route::get('/lesson/e/{id}', 'AtcTraining\CBTController@editLesson')->name('cbt.lesson.edit');
             Route::post('/lesson/edit/{id}', 'AtcTraining\CbtController@processEditLesson')->name('cbt.lesson.submit');
             Route::post('/examadmin/add/{id}', 'AtcTraining\CBTController@addQuestion')->name('cbt.exam.question.add');
             Route::post('/examadmin/update/{id}', 'AtcTraining\CBTController@updateQuestion')->name('cbt.exam.question.update');
