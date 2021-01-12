abfaf676591ff46e7df90b6626e2127597dfedc5
James White <jamesdwhite95@gmail.com>
2021-01-12 09:02:18 -0500

    Removed card from viewmodule, it broke HTML parcing

--- resources/views/dashboard/training/CBT/viewmodule.blade.php
+++ resources/views/dashboard/training/CBT/viewmodule.blade.php
@@ -20,17 +20,17 @@
           <h2 class="font-weight-bold blue-text"> {{$currentlesson->name}}</h2>
         </div>
         <br>
-    
-    <div class="card p-3"
+
+
       {{$currentlesson->html()}}
-      
+
       <br><br>
 
     @if($currentlesson->lesson == 'conclusion')
         <a class="btn btn-success href="{{route('cbt.module.complete', $currentlesson->cbt_modules_id)}}">Click To Mark Module As Completed!</a>
     @endif
 
-    </div>
+
   </div>
 </div>
 <br>
