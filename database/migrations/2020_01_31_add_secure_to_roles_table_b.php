9bf3f80b7e86dfb2e6ba1f22c5bc6665e245ca01
James White <jamesdwhite95@gmail.com>
2021-02-01 11:25:36 -0500

    1

--- database/migrations/2020_01_31_add_secure_to_roles_table_b.php
+++ database/migrations/2020_01_31_add_secure_to_roles_table_b.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddSecureToRolesTable extends Migration
+class SecureToRolesTableB extends Migration
 {
 /**
 * Run the migrations.
