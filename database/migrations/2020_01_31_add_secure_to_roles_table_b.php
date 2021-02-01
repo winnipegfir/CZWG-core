ac8bb2a81398b9b6f4ff8a937a80f4091e5de3c7
James White <jamesdwhite95@gmail.com>
2021-02-01 10:49:41 -0500

    Fix

--- database/migrations/2020_01_31_add_secure_to_roles_table_b.php
+++ database/migrations/2020_01_31_add_secure_to_roles_table_b.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddSecureToRolesTable extends Migration
+class AddSecureToRolesTableB extends Migration
 {
 /**
 * Run the migrations.
@@ -13,7 +13,7 @@ class AddSecureToRolesTable extends Migration
 */
 public function up()
 {
-Schema::create('roles', function (Blueprint $table) {
+    Schema::table('roles', function (Blueprint $table) {
 $table->integer('secure');
 });
 }
