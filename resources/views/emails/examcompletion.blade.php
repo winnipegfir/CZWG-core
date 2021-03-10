1a2ac015b4bee3f09bdee2ea9d083db22cb7e99e
CaptainWhite95 <66142198+CaptainWhite95@users.noreply.github.com>
2021-03-10 18:16:14 -0500

    Update examcompletion.blade.php

--- resources/views/emails/examcompletion.blade.php
+++ resources/views/emails/examcompletion.blade.php
@@ -6,10 +6,10 @@
 
 @section('message-content')
 @if ($grade >= 80)
-Your student {{$student->user->fullName('FLC')}} has passed the {{$exam}} with a grade of {{$grade}}%.
+Your student {{$student->user->fullName('FLC')}} has passed the {{$exam->name}} with a grade of {{$grade}}%.
 @endif
 @if ($grade <= 79)
-Your student {{$student->user->fullName('FLC')}} has failed the {{$exam}} with a grade of {{$grade}}%.
+Your student {{$student->user->fullName('FLC')}} has failed the {{$exam->name}} with a grade of {{$grade}}%.
 @endif
 <br>
 
