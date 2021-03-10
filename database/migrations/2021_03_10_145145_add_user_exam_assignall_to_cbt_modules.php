661ec313447334a26919bdacb17bbc1f0d3c83d0
CaptainWhite95 <66142198+CaptainWhite95@users.noreply.github.com>
2021-03-10 16:57:28 -0500

    Create 2021_03_10_145145_add_user_exam_assignall_to_cbt_modules.php

---
+++ database/migrations/2021_03_10_145145_add_user_exam_assignall_to_cbt_modules.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserExamAssignallToCbtModules extends Migration
+{
+/**
+* Run the migrations.
+*
+* @return void
+*/
+public function up()
+{
+Schema::table('cbt_modules', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('cbt_exam_id')->nullable()->unsigned();
+            $table->foreign('cbt_exam_id')->references('id')->on('cbt_exams');
+            $table->integer('assignall')->default('0');
+            
+        });
+}
+
+/**
+* Reverse the migrations.
+*
+* @return void
+*/
+public function down()
+{
+Schema::dropIfExists('roles');
+}
+}
