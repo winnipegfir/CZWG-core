8d6f994100eb74aaed7005d6b860c7af90caabac
James White <jamesdwhite95@gmail.com>
2021-03-24 12:38:42 -0400

    Test Push

--- config/app.php
+++ config/app.php
@@ -147,7 +147,6 @@ return [
         Illuminate\Session\SessionServiceProvider::class,
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
-        Illuminate\View\ViewServiceProvider::class,
 
         /*
          * Package Service Providers...
