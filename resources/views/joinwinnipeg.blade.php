869c40f82571910e5cb40b42fa15e629d84a05c0
Nate Power <natopower@gmail.com>
2022-02-04 13:55:58 -0400

    Updates to join page

--- resources/views/joinwinnipeg.blade.php
+++ resources/views/joinwinnipeg.blade.php
@@ -33,9 +33,9 @@
 					<ol>
 						<li>You will need to join the VATCAN division. If you have not already joined, or skipped some steps from Step 1, click the following link to set your region:<span> </span><a href="https://cert.vatsim.net/vatsimnet/regch.php">https://cert.vatsim.net/vatsimnet/regch.php</a>. You may proceed to step 5 if you are already a part of VATCAN.</li>
 						<li>You will now get your choice to select a region and division to join. To join VATCAN select<span> </span><strong>Americas</strong><span> </span>as your region.</li>
-						<li>In the select a division drop down box, select<span> </span><strong>Canada</strong></li>
-						<li>Press<span> </span><strong>Continue</strong><span> </span>to set your selection</li>
-						<li>Follow the steps on <a href="https://vatcan.ca/How-to-Become-a-Controller" target="_blank" rel="noopener noreferrer">https://vatcan.ca/How-to-Become-a-Controller</a> (if you already have your S1 (or higher) rating, you may skip step 2 on this page)</li>
+						<li>In the select a division drop down box, select<span> </span><strong>Canada</strong>.</li>
+						<li>Press<span> </span><strong>Continue</strong><span> </span>to set your selection.</li>
+						<li>Follow the steps on <a href="https://vatcan.ca/How-to-Become-a-Controller" target="_blank" rel="noopener noreferrer">https://vatcan.ca/How-to-Become-a-Controller</a> (if you already have your S1 (or higher) rating, you may skip step 2 on this page.)</li>
 					</ol>
 					<br>
 					<h3 class="font-weight-bold blue-text">Step 3 – Join the Winnipeg FIR</h3>
@@ -51,12 +51,13 @@
 				<div class="tab-pane fade" id="visit" role="tabpanel" aria-labelledby="visit"><br>
 					<div class="card">	
 						<div class="card-body bg-warning">
-							<text class="font-weight-bold">NOTE:</text> Winnipeg is currently pausing all training for visiting controllers as we are busy with home students. However, we continue to accept applications to visit! One of our staff members will be in touch when we are able to accommodate your training. Thank you for your patience.
+							<text class="font-weight-bold">NOTE:</text> Winnipeg is currently busy with a home controller wait list - this may result in lengthy delays for visiting controllers awaiting training. Controllers will be contacted with updates when available. Thank you for your patience.</text>
 						</div>
 					</div>
 					<br>
 					<p>Winnipeg is always looking for controllers to visit our FIR. Whether it's for a change of scenery, to learn a new way of controlling, or just for fun, come visit Winnipeg!</p>
-					<a class="btn btn-success" href="{{route('application.start')}}">Apply Now!</a>
+					<p>Members who are interested in visiting are now asked to visit the VATCAN.ca website to apply for visiting status.</p>
+					<a class="ml-0 btn btn-success" href="https://vatcan.ca/my/visit">Apply Now!</a>
 					</div>
 				</div>
 			</div>
@@ -64,7 +65,7 @@
 
 <br>
 <hr>
-	<h5 class="font-weight-bold blue-text">Curious how Winnipeg's training works?</h5>
+	<h5 class="font-weight-bold blue-text">View our estimated wait time and more.</h5>
 	<div class="row" style="padding-left:8px">
 		<a class="btn btn-sm btn-primary" href="{{route('training')}}">Click HERE to learn more!</a>
 	</div>
