ed6f70abb4c035c6a756a4a7c1d629c0cdc0a26f
natopower <natopower@gmail.com>
2023-09-14 13:01:52 -0600

    Quotes removed from 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,16 +4,8 @@
 @section('content')
     <div class="container py-5">
         <h1 class="font-weight-bold blue-text"><i class="fa fa-cogs"></i> Error 404.</h1>
-        <h4>Whatever you're looking for, we can't find it.</h4>
-        <p>In the meantime, here's a quote from our <a href="https://quotes.winnipegfir.ca/">Quotes Website!</a></p>
-        
-        <div class="card">
-          <div class="card-body">
-          <h4 class="blue-text" id="quote"></h4>
-          <p class="mb-0" id="name"></p>
-          </div>
-        </div>
-    </div>
+        <h4>Whatever you're looking for, we can't find it. Sorry, eh?</h4>
+        <a href="{{route('index')}}" class="blue-text" style="font-size: 1.2em"> <i class="fas fa-arrow-left"></i> Back to the homepage</a>
 
 <script>
 fetch("https://quotes.winnipegfir.ca/api/quotes/random")
