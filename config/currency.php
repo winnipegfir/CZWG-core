060c0cc9d058bdaab1f1e10ae0aee13ae7999268
Kolby Dunning <kolby.dunning@gmail.com>
2024-03-14 19:26:03 -0400

    Update currency.php

--- config/currency.php
+++ config/currency.php
@@ -3,6 +3,6 @@
 // Set required hours per month for each member on roster in here.
 return [
     'visit' => 1,
-    'home' => 2,
-    'instructor' => 3,
+    'home' => 1,
+    'instructor' => 1,
 ];
